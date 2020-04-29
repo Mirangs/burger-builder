d86875177924759dacfa8af60093f5072ee25006
Max Berezanskyi <berezanskij98@gmail.com>
2020-04-29 14:52:49 +0300

    Added session auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { decode } from 'jsonwebtoken';
+import { useDispatch } from 'react-redux';
 
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -7,21 +9,34 @@ import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 
+import { authSuccess } from './store/actions/auth';
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const idToken = sessionStorage.getItem('idToken');
+    const decoded = idToken && decode(idToken);
+
+    if (decoded) {
+      dispatch(authSuccess(idToken, decoded.user_id));
+    }
+  }, [dispatch]);
+
   return (
     <>
-    <BrowserRouter>
-      <Layout>
+      <BrowserRouter>
+        <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
             <Route path="/auth" component={Auth} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
-      </Layout>
-    </BrowserRouter>
+        </Layout>
+      </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -3,54 +3,58 @@ import axios from 'axios';
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => ({
-  type: actionTypes.AUTH_START
+  type: actionTypes.AUTH_START,
 });
 
 export const authSuccess = (idToken, userId) => ({
   type: actionTypes.AUTH_SUCCESS,
   idToken,
-  userId
+  userId,
 });
 
-export const authFail = err => ({
+export const authFail = (err) => ({
   type: actionTypes.AUTH_FAIL,
-  err
+  err,
 });
 
 export const logout = () => ({
-  type: actionTypes.AUTH_LOGOUT
+  type: actionTypes.AUTH_LOGOUT,
 });
 
-export const checkAuthTimeout = expirationTime => {
-  return dispatch => {
+export const checkAuthTimeout = (expirationTime) => {
+  return (dispatch) => {
     setTimeout(() => {
       dispatch(logout());
     }, expirationTime * 1000);
   };
-}
+};
 
 export const auth = (email, password, isSignup) => {
-  return dispatch => {
+  return (dispatch) => {
     dispatch(authStart());
     const authData = {
       email,
       password,
-      returnSecureToken: true 
+      returnSecureToken: true,
     };
 
-    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCK4DjjCMvX9iojDU7Ptnxqeki2yovdiXE';
+    let url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCK4DjjCMvX9iojDU7Ptnxqeki2yovdiXE';
 
     if (!isSignup) {
-      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCK4DjjCMvX9iojDU7Ptnxqeki2yovdiXE';
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCK4DjjCMvX9iojDU7Ptnxqeki2yovdiXE';
     }
 
-    axios.post(url, authData)
-      .then(res => {
+    axios
+      .post(url, authData)
+      .then((res) => {
+        sessionStorage.setItem('idToken', res.data.idToken);
         dispatch(authSuccess(res.data.idToken, res.data.localId));
         dispatch(checkAuthTimeout(res.data.expiresIn));
       })
-      .catch(err => {
+      .catch((err) => {
         dispatch(authFail(err));
-      })
+      });
   };
-}
+};
